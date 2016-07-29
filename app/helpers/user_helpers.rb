775fd6587b0619a2b8d7d2d4a26c0c7f80d413b6
Christopher Danella <christopher.danella@gmail.com>
2016-07-29 11:52:00 -0400

    Add nav bar to homepage

---
+++ app/helpers/user_helpers.rb
@@ -0,0 +1,15 @@
+helpers do
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def redirect_nologin
+    redirect '/' unless logged_in?
+  end
+
+end
